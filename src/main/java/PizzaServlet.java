9aefa703c1923a70b749dee0c1f64304d3e4efe3
Casanova Geary <CasanovaGeary@Gmail.com>
2022-11-02 16:34:34 -0500

    PizzaServlet completed.

---
+++ src/main/java/PizzaServlet.java
@@ -0,0 +1,25 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Arrays;
+
+@WebServlet(name = "PizzaServlet", urlPatterns = "/pizza-order")
+public class PizzaServlet extends HttpServlet
+{
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException
+    {
+        request.getRequestDispatcher("/pizza-order.jsp").forward(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException
+    {
+        System.out.println(request.getParameter("location"));
+        System.out.println(request.getParameter("sauce"));
+        System.out.println(request.getParameter("crust"));
+        System.out.println(request.getParameter("size"));
+        System.out.println(Arrays.toString(request.getParameterValues("veggies")));
+    }
+}
