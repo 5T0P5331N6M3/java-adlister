cd29d672252231d320bf43afce7c2c5a8be605fb
Casanova Geary <CasanovaGeary@Gmail.com>
2022-11-07 14:16:37 -0600

    Created index.jsp showing all ads on site.

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -6,11 +6,13 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebServlet(name = "LoginServlet", urlPatterns = "/login")
+@WebServlet("/login")
 public class LoginServlet extends HttpServlet
 {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
+        request.getRequestDispatcher("/login.jsp").forward(request, response);
         String username = request.getParameter("username");
         String password = request.getParameter("password");
 
@@ -18,13 +20,22 @@ public class LoginServlet extends HttpServlet
         System.out.println("Username: " + username);
         System.out.println("Password: " + password);
 
-        if (username != null && username.equalsIgnoreCase("admin@stuff.com") && password != null && password.equalsIgnoreCase("admiin"))
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        if (request.getMethod().equalsIgnoreCase("post"))
         {
-            // Redirect to a Welcome Page; need to pass value in session in order to carry forward one to next pages
-            HttpSession httpSession = request.getSession();
-            // By setting the variable in session, it can be forwarded
-            httpSession.setAttribute("username", username);
-            request.getRequestDispatcher("login.jsp").forward(request, response);
+            String username = request.getParameter("username");
+            String password = request.getParameter("password");
+            if (username.equals("admin") && password.equals("password"))
+            {
+                response.sendRedirect("/profile");
+            } else
+            {
+                response.sendRedirect("/login");
+            }
         }
     }
 }
--- src/main/webapp/ads/index.jsp
+++ src/main/webapp/ads/index.jsp
@@ -1,3 +1,4 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%--
   Created by IntelliJ IDEA.
   User: casanovageary
@@ -12,5 +13,11 @@
 </head>
 <body>
     <h1>List of All Ads on This Site</h1>
+    <c:forEach var="ad" items="${ads}">
+        <div class="ad">
+            <h2>${ad.title}</h2>
+            <p>${ad.description}</p>
+        </div>
+    </c:forEach>
 </body>
 </html>
