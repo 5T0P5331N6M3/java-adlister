f259391f0fa74b4d5ae1bf2406d054e95d56c57a
Casanova Geary <CasanovaGeary@Gmail.com>
2022-11-12 22:52:45 -0600

    Created bean Album.

---
+++ src/main/java/Album.java
@@ -0,0 +1,72 @@
+import java.time.LocalDate;
+import java.util.Date;
+
+public class Album
+{
+    private long id;
+    private String artist;
+    private String record_name;
+    private LocalDate release_date;
+    private double sales;
+    private String genre;
+
+    public Album(){}
+
+    public Album(long id, String artist, String record_name, LocalDate release_date, double sales, String genre)
+    {
+        this.id = id;
+        this.artist = artist;
+        this.record_name = record_name;
+        this.release_date = release_date;
+        this.sales = sales;
+        this.genre = genre;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getRecord_name() {
+        return record_name;
+    }
+
+    public void setRecord_name(String record_name) {
+        this.record_name = record_name;
+    }
+
+    public LocalDate getRelease_date() {
+        return release_date;
+    }
+
+    public void setRelease_date(LocalDate release_date) {
+        this.release_date = release_date;
+    }
+
+    public double getSales() {
+        return sales;
+    }
+
+    public void setSales(double sales) {
+        this.sales = sales;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+}
